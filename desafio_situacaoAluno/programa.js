401d26e68b90f3462b478e8207e0a235ff23b658
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-06-13 00:40:06 -0300

    update

--- desafio_situacaoAluno/programa.js
+++ desafio_situacaoAluno/programa.js
@@ -8,17 +8,17 @@ function obterNome(){
 }
 function formatNome(nome){
     let nome1 = [];
-    nomes_separados = nome.split(" ");
-    for(nome of nomes_separados){
+    let nomes_separados = nome.split(" ");
+    for(let nome of nomes_separados){
         nome = nome[0].toUpperCase() + nome.substring(1);
         nome1.push(nome);
     }
-    nomes_juntos = nome1.join(" ");
+    let nomes_juntos = nome1.join(" ");
     return nomes_juntos;
 }
 function obterNota(nome_ativ ,nome_aluno){
     let nota = Number(window.prompt(`Digite a nota ${nome_ativ} de ${nome_aluno}: `));
-    while(isNaN(nota)){
+    while(isNaN(nota) || nota < 0 || nota > 10){
         window.alert("Opção Invalida!");
         return obterNota(nome_ativ ,nome_aluno);
     }
@@ -31,21 +31,51 @@ function notasN1(nome_aluno){
         let nota = obterNota(nome, nome_aluno);
         notas.push(nota);
     }
-    return notas;
+    return (somar(notas) / 4) * 0.4;
 }
-//
-function somarArray(array) {
+function notaN2(nome_aluno){
+    let nota = obterNota("N2", nome_aluno);
+    return nota * 0.6;
+}
+function notaFinal(nota_n1, nota_n2){
+    let nota = nota_n1 + nota_n2;
+    return nota;
+}
+/*function somarArray(array) {
     return array.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+}*/
+function somar(array){
+    let soma = 0;
+    for(let nota of array){
+        soma += nota;
+    }
+    return soma;
 }
-//nota final da n1
-
-function respostas(nome, notas_n1){
+function formatValor(v){
+    return new Intl.NumberFormat("pt-br", {minimumFractionDigits: 1, maximumFractionDigits: 1}).format(v);
+}
+function respostas(nome, notas_n1, nota_n2, nota_fin){
     let resposta = window.document.querySelector("p#resposta");
     resposta.innerHTML = `<h2>Analisando a situação de ${nome}...</h2>`;
-    resposta.innerHTML += `<p>${notas_n1}</p>`;
+    resposta.innerHTML += `<p>Com as notas ${formatValor(notas_n1)} & ${formatValor(nota_n2)} a <b>média é ${formatValor(nota_fin)}</b></p>`;
+    respFinal(nota_fin);
+}
+function respFinal(nota_fin){
+    let resposta = window.document.querySelector("div#resposta2");
+    if (nota_fin < 3.0){
+        resposta.innerHTML = "<p>Com média abaixo de 3,0 o aluno esta REPROVADO!</p>";
+    }
+    else if (nota_fin >= 3.0 && nota_fin < 6.0){
+        resposta.innerHTML = "<p>Com média entre 3,0 e 6,0 o aluno esta em RECUPERAÇÃO!</p>";
+    }
+    else{
+        resposta.innerHTML = "<p>Com média acima de 6,0 o aluno esta APROVADO!</p>";
+    }
 }
 function iniciar(){
     let nome = obterNome();
     let notas_n1 = notasN1(nome);
-    respostas(nome, notas_n1);
+    let nota_n2 = notaN2(nome);
+    let nota_final = notaFinal(notas_n1, nota_n2);
+    respostas(nome, notas_n1, nota_n2, nota_final);
 }
